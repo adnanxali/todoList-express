8168848d28d644c5d9066d13e381badcc8f22c58
Adnan <adnanali11875@gmail.com>
2023-10-06 12:13:29 +0530

    progress

---
+++ public/javascripts/homepage.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.task').forEach(task=>{
+    const checkbox=task.querySelector('.task-done');
+    checkbox.addEventListener('click',()=>{
+    if(checkbox.checked){
+        task.querySelector('.task-link').classList.toggle('strike-thr');
+    }
+    else if(!checkbox.checked){
+        task.querySelector('.task-link').classList.toggle('strike-thr');
+        
+    }
+        })
+    })
+
+
